76bf803fede6772165e4f1a9ba023135605da96b
91 <hatelove@users.noreply.github.com>
2015-10-18 17:05:35 +0800

    Update GroupPagingServiceTests.cs
    請看 pull request 的註解差異。
    
    第二個 test case 的修正同第一個 test case。
    
    不應該是兩個方法：SumGoodsCost() 跟 SumGoodsRevenue(), 這樣未來如果 Goods 有新的欄位，就得新開一個方法，相當沒有彈性，也與需求不符。
    
    可以有更漂亮的作法，看您想不想再調整看看。

--- Rex.Skill.Tdd.Tests/GroupPaging/GroupPagingServiceTests.cs
+++ Rex.Skill.Tdd.Tests/GroupPaging/GroupPagingServiceTests.cs
@@ -9,6 +9,7 @@ namespace Rex.Skill.Tdd.Tests.GroupPaging
     [TestClass]
     public class GroupPagingServiceTests
     {
+        // target 這個命名建議給測試目標用，如果是 List<Godds>, 建議就命成 goods 即可
         private List<Goods> _target;
 
         [TestInitialize]
@@ -16,6 +17,8 @@ namespace Rex.Skill.Tdd.Tests.GroupPaging
         {
             _target = new List<Goods>()
             {
+                // 建議不要使用多參數的 constructor, 因為這個 homework 是要練習怎麼用 test case 來描述需求跟 context
+                // 建議直接使用 property setter, 例如：new Goods {Id=1, Cost=1, Revenue=11, SellPrice=21}，原因是讓看的人一目了然
                 new Goods( 1,  1, 11 , 21),
                 new Goods( 2,  2, 12 , 22),
                 new Goods( 3,  3, 13 , 23),
@@ -40,11 +43,14 @@ namespace Rex.Skill.Tdd.Tests.GroupPaging
         public void SumGoodsCostTest_3筆一組_取Cost總和()
         {
             List<int> expected = new List<int>() { 6, 15, 24, 21 };
-
+            
             IGroupPagingContext context = Substitute.For<IGroupPagingContext>();
             context.Goodies.Returns(_target);
-
+            
+            // 既然是測試 GroupPagingService，建議宣告的部分就不要用 IGroupPagingService，在所有的單元測試中，測試目標的宣告都不需要用 interface, 因為我要測的就是測試目標，而不是介面。
             IGroupPagingService service = new GroupPagingService(context);
+            
+            // SumGoodsCost(int pageSize)的方法比較沒有彈性，需求是希望用同一個方法，就可以讓呼叫端決定這一次要「幾筆一組」，「取哪個欄位的總和」
             List<int> actual = service.SumGoodsCost(3).ToList();
 
             CollectionAssert.AreEqual(expected, actual);
