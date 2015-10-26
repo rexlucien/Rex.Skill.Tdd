03167b58616a8eb5d1dacc15b2bf62b1f9f20dcd
rexlucien <rexlucien@hotmail.com>
2015-10-27 00:57:34 +0800

    第六個綠燈

--- Rex.Skill.Tdd/PotterShoppingCart/PotterShoppingCart.cs
+++ Rex.Skill.Tdd/PotterShoppingCart/PotterShoppingCart.cs
@@ -13,8 +13,27 @@ namespace Rex.Skill.Tdd.PotterShoppingCart
     {
         public decimal Caculate(List<Book> books)
         {
-            int disconut = books.Count >= 4 ? books.Count + 1 : books.Count;
-            return Convert.ToDecimal(books.Sum(x => (double)x.Price) * (1.05 - disconut * 0.05));
+            List<Book> source = books;
+
+            decimal total = 0;
+
+            while (source.Any())
+            {
+                var ids = source.GroupBy(x => x.Id).Select(x => x.Key);
+
+                List<Book> sub = new List<Book>();
+
+                foreach (var temp in ids.Select(key => source.FirstOrDefault(x => x.Id == key)))
+                {
+                    if (temp != null) sub.Add(temp);
+                    source.Remove(temp);
+                }
+
+                int disconut = sub.Count >= 4 ? sub.Count + 1 : sub.Count;
+                total += Convert.ToDecimal(sub.Sum(x => (double)x.Price) * (1.05 - disconut * 0.05));
+            }
+
+            return total;
         }
     }
 
